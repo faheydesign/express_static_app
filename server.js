d76f53eeb15c95e7fdd137524a5495e1eb5de4bc
John Fahey <faheydesign@gmail.com>
2014-08-07 13:31:16 -0700

    Adding solution for express static server

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static('public'));
+
+
+app.get('/', function(req, res){
+  res.sendFile("/public/index.html");
+  console.log("serving up index");
+});
+
+
+app.listen(3000, function () {
+  console.log("magic happens on port 3000");
+});
